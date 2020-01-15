298d441010fc68d135222bccb2eade0e7440b1c9
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-15 23:53:06 +0530

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -6,7 +6,7 @@ import Img from 'gatsby-image'
 
 import { Navigation } from '.'
 import config from '../../utils/siteConfig'
-import config2 from '../../common/analytics'
+import config2 from '../../custom/analytics'
 
 // Styles
 import '../../styles/app.css'
