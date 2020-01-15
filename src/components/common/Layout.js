91ec5e6cda6cbe2ad0b31da2bf4da1b36143dcb3
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-15 22:48:43 +0530

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -10,6 +10,27 @@ import config from '../../utils/siteConfig'
 // Styles
 import '../../styles/app.css'
 
+
+class App extends Component {
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js";
+    this.div.appendChild(script);
+  }
+  render() {
+    return (
+      <div className="App" ref={el => (this.div = el)}>
+        <h1>Hello react</h1>
+        {/* Script is inserted here */}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
 /**
 * Main layout component
 *
