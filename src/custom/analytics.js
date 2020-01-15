99ee662c55b0da1fb889f314f06931de7b019b5a
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-15 23:21:05 +0530

    Delete analytics.js

--- src/custom/analytics.js
+++
@@ -1,7 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-85412535-3"></script>
-<script type="text/javascript">
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-85412535-3');
-</script>
