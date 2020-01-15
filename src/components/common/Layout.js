168a2528e240fa8b64e27eaf2fb55040dbb02877
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-15 20:21:52 +0530

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -28,6 +28,15 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
             <Helmet>
                 <html lang={site.lang} />
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
+                
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-85412535-3"></script>
+                <script type="text/javascript">
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'UA-85412535-3');
+                </script>
+
                 <body className={bodyClass} />
             </Helmet>
 
