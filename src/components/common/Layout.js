78598f594cd4442b69569c6d3e8af7d406750fe8
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-15 13:23:57 +0530

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -54,8 +54,8 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                             </div>
                             { isHome ?
                                 <div className="site-banner">
+                                    <h1 className="site-banner-title">{site.title}</h1>
                                     <p className="site-banner-desc">{site.description}</p>
-                                    <p><a style={{ { backgroundImage: `url(${site.cover_image})` } }} to="/"><span>UILO</span></a></p>
                                 </div> :
                                 null}
                             <nav className="site-nav">
