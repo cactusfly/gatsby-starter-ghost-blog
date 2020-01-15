098d3e9b99e78f903ec3021aa20005b79039f0f4
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-16 00:12:34 +0530

    Update analytics.js

--- src/custom/analytics.js
+++ src/custom/analytics.js
@@ -1,1 +1,9 @@
-alert( 'Hello, world!' );
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-85412535-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-85412535-3');
+</script>
