acdd91efa2570a941440bf58bd540194320aac07
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-16 00:17:06 +0530

    Delete custom_2020.js

--- src/utils/custom_2020.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-React.createElement("script", {
-  type: "text/jsx"
-}, "/** @jsx React.DOM */ React.renderComponent(", React.createElement("h1", null, "Hello, world!"), ", document.getElementById('blog-uilo-search') );");
