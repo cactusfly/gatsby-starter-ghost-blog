675a5fbdbae524596ad72e55cd800092dd7ec33f
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-15 20:19:42 +0530

    Create custom_2020.js

---
+++ src/utils/custom_2020.js
@@ -0,0 +1,5 @@
+"use strict";
+
+React.createElement("script", {
+  type: "text/jsx"
+}, "/** @jsx React.DOM */ React.renderComponent(", React.createElement("h1", null, "Hello, world!"), ", document.getElementById('blog-uilo-search') );");
