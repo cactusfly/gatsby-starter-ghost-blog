20f1488116814c8eab0e0a30d16cac1cd81967ed
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-16 08:11:10 +0530

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -60,9 +60,9 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                             { isHome ?
                                 <div className="site-banner">
                                     {/* <h1 className="site-banner-title">{site.title}</h1> */}
-                                    <center><a style={{textDecoration: 'none !important'}} href="/"><p style={{marginTop: '30px', color: '#000', fontSize: '50px', marginBottom: 0, textTransform: 'uppercase', transform: 'rotate(-20deg) skew(-20deg)', backgroundColor: 'transparent', fontFamily: '"MyGillSans", Gill Sans MT !important', fontWeight: 'bold'}}><span style={{lineHeight: '1.25', marginBottom: 0, backgroundColor: 'white', color: 'black', position: 'relative', zIndex: 9999, fontFamily: '"MyGillSans" , Gill Sans MT !important', textAlign: 'center'}}>UILO</span><br /></p><p className="em-zap" /></a></center> <br />
+                                    <center><a style={{textDecoration: 'none !important'}} href="/"><p style={{marginTop: '30px', color: '#000', fontSize: '50px', marginBottom: 0, textTransform: 'uppercase', transform: 'rotate(-20deg) skew(-20deg)', backgroundColor: 'transparent', fontWeight: 'bold'}}><span style={{lineHeight: '1.25', marginBottom: 0, backgroundColor: 'white', color: 'black', position: 'relative', zIndex: 9999, textAlign: 'center'}}>UILO</span><br /></p><p className="em-zap" /></a></center> <br />
                                     {/* <p className="site-banner-desc">{site.description}</p> */}
-                                    <div style={{fontSize: '1.25em', textTransform: 'uppercase', fontFamily: '"MyGillSans", Gill Sans MT !important', marginRight: '0px', fontWeight: 'normal', letterSpacing: '2.25px', color: 'slategrey !important'}} id="changeText" />
+                                    <div style={{fontSize: '1.25em', textTransform: 'uppercase', marginRight: '0px', fontWeight: 'normal', letterSpacing: '2.25px', color: 'slategrey !important'}} id="changeText" />
                                 </div> :
                                 null}
                             <nav className="site-nav">
