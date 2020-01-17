ef966ce1b53e16dd8d3565954418a37cfcdb5ee2
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-17 08:43:05 +0530

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -32,7 +32,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <meta name="description" content="UILO" />
                 <meta name="keywords" content="sales enablement, sales outsourcing" />    
                 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-                <body className={bodyClass} />
+                <body className="home-template" {bodyClass} />
             </Helmet>           
                 
             <div className="viewport">
