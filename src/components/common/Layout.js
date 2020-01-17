bb706975a234b59d07f59cd04f1d8419cd034a4c
cactusfly <59895633+cactusfly@users.noreply.github.com>
2020-01-17 20:45:21 +0530

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -41,15 +41,14 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     {/* The main header section on top of the screen */}
                     <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
                     
-                    <script
-                      dangerouslySetInnerHTML={{
-                        __html: `
-                            var body = document.body;
-                            body.classList.add("home-template");
+                            <script
+                              dangerouslySetInnerHTML={{
+                                __html: `
                             function change() {
-                                   var elem = document.getElementById("changeText");
+                                var elem = document.getElementById("changeText");
                                 var myArray = ["Outsourced Sales Development", "Fractional Sales Leadership", "Sales Development Consulting", "Sales Enablement Consulting", "Sales Training", "Enterprise Selling", "Big Ticket Sales Coaching", "Sales Assessments", "Sales Management Training", "Sales Alignment", "Growth Consulting", "Hiring for Sales", "Leadership Coaching", "Sales Conversations", "Sales Productivity", "Sales Resources", "Business Strategy", "Customer Value Marketing", "Lead Nurturing", "Lead Generation", "Lead Scoring", "Customer Experience", "Real-time Sales Personalization", "Sales Intelligence", "Inbound Marketing", "CX Lifecycle Management", "Customer Experience!", "Big Data Marketing", "Deep Data Integration", "Analytics Automation", "Transaction Automation", "Marketing Automation", "Multichannel Marketing", "Dynamic Lead Generation", "Sales Automation", "Predictive marketing", "Customer Referrals", "Responsive Sales", "Growth Hacking!", "CRM Integrations", "ERP Integrations", "Sales Development", "Sales Engagement", "Sales Management", "Sales Operations", "Sales Process", "Sales Technology"];
-                                elem.innerHTML = myArray[Math.floor(Math.random() * myArray.length)]; } 
+                                try { elem.innerHTML = myArray[Math.floor(Math.random() * myArray.length)]; } }
+                                catch { console.log('err1'); }
                             function homePageOnly() { var inst = setInterval(change, Math.floor(Math.random() * 2000) + 5000); }
                             if(window.location.pathname == "/"){ homePageOnly(); }
                             `, }} />
@@ -66,7 +65,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                     </Link>
                                 </div>
                                 <div className="site-mast-right">
-                                    { isHome ? <div id="blog-uilo-search" style={{fontSize: '1.2em !important'}} className="input-drop"> <input type="text" placeholder="Search..." /> <ul /> </div> : null }
+                                    <div id="blog-uilo-search" style={{fontSize: '1.2em !important'}} className="input-drop"> <input type="text" placeholder="Search..." /> <ul /> </div> 
                                  </div>
                             </div>
                             { isHome ?
